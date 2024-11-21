038fddd52982a1795e1091ce0be573adf0afc9c6
Joao Brosselin <166879336+sfbrosselin@users.noreply.github.com>
2024-11-21 19:18:10 -0300

    a

--- src/components/NavBarModal.js
+++ src/components/NavBarModal.js
@@ -35,7 +35,7 @@ export default function NavBarModal(){
         <ModalContents>
             {location.pathname !== "/localizador" && <Icons className="fa-solid fa-map" to = {"/coletas"}> COLETAS </Icons>}
             {location.pathname !== "/guia" && <Icons className="fa-solid fa-recycle" to = {"/guia"} > Guia </Icons>}
-            {location.pathname !== "/sobre" && <Icons className="fa-solid fa-circle-info" to = {"/sobre"}> Sobre </Icons>} 
+            {/* {location.pathname !== "/sobre" && <Icons className="fa-solid fa-circle-info" to = {"/sobre"}> Sobre </Icons>}  */}
         </ModalContents>
     )
 }
